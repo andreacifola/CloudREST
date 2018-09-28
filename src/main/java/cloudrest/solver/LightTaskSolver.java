a2abc677717eedf28c3f32462b21f067e536e55d
Andrea Cifola <cifola.andrea@gmail.com>
2018-09-28 09:53:22 +0200

    Aggiunte directories "utils" e "solver" con relative classi di appartenenza

---
+++ src/main/java/cloudrest/solver/LightTaskSolver.java
@@ -0,0 +1,13 @@
+package cloudrest.solver;
+
+import fognoderest.entities.LightTask;
+import fognoderest.utils.UtilityMD5;
+
+
+public class LightTaskSolver {
+
+    public String hash(LightTask task){
+        String encrypted = UtilityMD5.stringByHashingPassword(task.getToEncrypt());
+        return encrypted;
+    }
+}
