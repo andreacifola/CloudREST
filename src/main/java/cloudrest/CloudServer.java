c555b15c8f8565829895b0edeac0091dc4d8778b
Andrea Cifola <cifola.andrea@gmail.com>
2018-09-28 09:35:34 +0200

    Aggiunto il run del server

---
+++ src/main/java/cloudrest/CloudServer.java
@@ -0,0 +1,12 @@
+package cloudrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServer.class, args);
+	}
+}
