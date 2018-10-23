eff36d3e97f18984216b6eb84da05dc62ab9d831
simone <jaded@live.it>
2018-10-23 14:49:15 +0200

    solver del light task corretto

--- src/main/java/cloudrest/solver/LightTaskSolver.java
+++ src/main/java/cloudrest/solver/LightTaskSolver.java
@@ -17,10 +17,18 @@ public class LightTaskSolver {
         int i;
         for (i = loopCount+1; i < toEncrypt.length(); i++) {
             char letter = toEncrypt.charAt(i);
+
             if (Character.isLetter(letter)) {
-                letter += 3;
+                if(letter == 'x')
+                    letter = 'a';
+                else if(letter == 'y')
+                    letter = 'b';
+                else if(letter == 'z')
+                    letter = 'c';
+                else
+                    letter += 3;
+                encrypted += letter;
             }
-            encrypted += letter;
         }
         return encrypted;
     }
