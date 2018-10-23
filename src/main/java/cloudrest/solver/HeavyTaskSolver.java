2933bbb4d3fd961cd70cb6c372fbcd3f1f86d17e
simone <jaded@live.it>
2018-10-23 15:21:50 +0200

    solver heavy task pulito

--- src/main/java/cloudrest/solver/HeavyTaskSolver.java
+++ src/main/java/cloudrest/solver/HeavyTaskSolver.java
@@ -6,25 +6,15 @@ import java.io.IOException;
 import java.math.BigInteger;
 
 public class HeavyTaskSolver {
-/*
-    public BigInteger factorial(HeavyTask heavyTask, int n, BigInteger partial, int last) {
-            int i;
-            BigInteger fact = partial;  //setto il risultato come il parziale eventuale di una esecuzione precedente
-            for (i = last + 1; i <= n; i++)
-                fact = fact.multiply(BigInteger.valueOf(i));
 
-            System.out.println("fattoriale di n = " + n + ": " + fact);
-            return fact;
+    public BigInteger factorial(HeavyTask heavyTask, int n, BigInteger partial, int last){
+        int i;
+        BigInteger fact = partial;  //setto il risultato come il parziale eventuale di una esecuzione precedente
+        System.out.println("fact ricevuto : " + fact);
+        for(i = last+1; i<=n; i++)
+            fact = fact.multiply(BigInteger.valueOf(i));
+
+        System.out.println("fattoriale di n = " + n + ": " + fact);
+        return fact;
     }
-*/
-public BigInteger factorial(HeavyTask heavyTask, int n, BigInteger partial, int last){
-    int i;
-    BigInteger fact = partial;  //setto il risultato come il parziale eventuale di una esecuzione precedente
-    System.out.println("fact ricevuto : " + fact);
-    for(i = last+1; i<=n; i++)
-        fact = fact.multiply(BigInteger.valueOf(i));
-
-    System.out.println("fattoriale di n = " + n + ": " + fact);
-    return fact;
-}
 }
